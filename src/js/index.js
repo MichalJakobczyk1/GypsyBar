7516d16cedb076d2e3b7ac1fca8e9d092c146cbf
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2023-04-13 12:28:22 +0200

    observer animations,gallery fix

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,78 @@ import "../scss/main.scss";
 
 /* place your code below */
 
+const aboutSection = document.querySelectorAll(".section--about");
+const aboutHeading = document.querySelector(".about__h2--js");
+const aboutParagraph = document.querySelector(".about__paragraph--js");
+
+const gallerySection = document.querySelectorAll(".section--gallery");
+const galleryHeading = document.querySelector(".gallery__h2--js");
+const galleryParagraph = document.querySelector(".gallery__paragraph--js");
+
+const contactSection = document.querySelectorAll(".section--contact");
+const contactParagraphOne = document.querySelector(".contact__paragraph--1");
+const contactParagraphTwo = document.querySelector(".contact__paragraph--2");
+const contactParagraphThree = document.querySelector(".contact__paragraph--3");
+const contactParagraphFour = document.querySelector(".contact__paragraph--4");
+const contactForm = document.querySelector(".contact__container--form");
+
+const aboutObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        aboutHeading.style.animation = `appearDown 1s 0.75s forwards`;
+        aboutParagraph.style.animation = `appearDown 1s 1s forwards`;
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+const galleryObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        galleryHeading.style.animation = `appearDown 1s 0.75s forwards`;
+        galleryParagraph.style.animation = `appearDown 1s 1s forwards`;
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+const contactObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        contactParagraphOne.style.animation = `appearDown 1s 0.5s forwards`;
+        contactParagraphTwo.style.animation = `appearDown 1s 0.75s forwards`;
+        contactParagraphThree.style.animation = `appearDown 1s 1s forwards`;
+        contactParagraphFour.style.animation = `appearDown 1s 1.25s forwards`;
+        contactForm.style.animation = `appearDown 1s 1.75s forwards`;
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+aboutSection.forEach((about) => {
+  aboutObserver.observe(about);
+});
+
+gallerySection.forEach((gallery) => {
+  galleryObserver.observe(gallery);
+});
+
+contactSection.forEach((contact) => {
+  contactObserver.observe(contact);
+});
+
 const main = document.querySelector(".main--preload");
 
 function remove() {
