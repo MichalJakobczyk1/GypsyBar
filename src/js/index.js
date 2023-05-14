f36ab840b97ca4e3dbf09ac5e5e39549a5ab489e
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2023-05-14 19:26:10 +0200

    tablet media fix&new photos

--- src/js/index.js
+++ src/js/index.js
@@ -109,6 +109,8 @@ const buttonEng = document.querySelector(".lang__button--eng");
 const navAbout = document.querySelector(".navigation__link--about");
 const navGallery = document.querySelector(".navigation__link--gallery");
 const navContact = document.querySelector(".navigation__link--contact");
+const welcomeH1 = document.querySelector(".welcome__h1--js");
+const welcomePar = document.querySelector(".welcome__paragraph--js");
 const aboutPar = document.querySelector(".about__paragraph--js");
 const galleryH2 = document.querySelector(".gallery__h2--js");
 const galleryPar = document.querySelector(".gallery__paragraph--js");
@@ -128,13 +130,15 @@ buttonLang.addEventListener("click", () => {
     navAbout.innerHTML = "O mnie";
     navGallery.innerHTML = "Galeria";
     navContact.innerHTML = "Kontakt";
+    welcomeH1.innerHTML = "Najlepsze koktajlowe doświadczenie";
+    welcomePar.innerHTML = "Poczuj atmosferę koktajlbaru na swoim przyjęciu!";
     aboutPar.innerHTML =
-      "Aliquam rutrum ultricies eros ut porttitor. Aliquam erat volutpat. Cras ac metus tincidunt, iaculis ipsum in, sodales nibh. Fusce eu sem ut eros scelerisque bibendum. Cras commodo magna a dui rutrum pulvinar. Duis condimentum et mauris nec posuere. Vestibulum porta metus vel magna aliquet, non tristique elit ultricies.";
+      "Wyobraź sobie, że popijasz pyszny koktajl, przygotowany ze świeżych składników i dostosowany do Twoich preferencji smakowych, a wszystko to w towarzystwie Twoich przyjaciół i rodziny. Z Nami nie musisz się martwić o logistykę ustawienia baru lub o to, że zabraknie Ci napojów. Zajmiemy się wszystkim, od zapewnienia sprzętu barowego i jego ustawienia, po zaopatrzenie w alkohol i napoje. Skorzystanie z oferty Gypsy’s to jak posiadanie osobistego koktajlbaru, który przyjeżdża do Ciebie! To nie są zwykli barmani, ale doświadczeni i pełni pasji eksperci, którzy są oddani zapewnieniu Tobie i Twoim gościom niezapomnianych wrażeń. Niezależnie od tego czy jest to ślub, impreza firmowa, prywatne przyjęcie czy festiwal, ożywimy imprezę dzięki niestandardowemu menu drinków, fachowo przygotowanym koktajlom i wyjątkowej obsłudze!";
     galleryH2.innerHTML = "Galeria";
     galleryPar.innerHTML = "Sprawdź moje poprzednie realizacje";
     contactParagraphOne.innerHTML = "Sprawdź nowe posty";
     contactParagraphTwo.innerHTML = "Oraz zdjęcia i relacje";
-    contactH3.innerHTML = "Lorem ipsum lorem ipsum";
+    contactH3.innerHTML = "Lub skontaktuj się </br> ze mną od razu";
     submit.innerHTML = "Wyślij";
     namePlaceholder.placeholder = "Imię";
     emailPlaceholder.placeholder = "Email";
@@ -147,13 +151,15 @@ buttonLang.addEventListener("click", () => {
     navAbout.innerHTML = "About";
     navGallery.innerHTML = "Gallery";
     navContact.innerHTML = "Contact";
+    welcomeH1.innerHTML = "Best cocktail experience";
+    welcomePar.innerHTML = "Feel cocktail bar experience on your party!";
     aboutPar.innerHTML =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, metus vitae consectetur tempor, dui nulla viverra turpis, et fringilla ex felis quis erat. Suspendisse feugiat lacinia nibh. Aenean et tortor dapibus, varius tortor non, semper sapien. Nullam eu risus quis nibh pellentesque porta. Donec sodales ipsum nisi, eu rhoncus libero egestas vel.";
+      "Imagine sipping a delicious cocktail, made with fresh ingredients and tailored to your taste preferences, all in the company of Your friends and family. With us, You don't have to worry about the logistics of setting up your bar or running out of drinks. We'll take care of everything, from providing bar equipment and set-up, to sourcing alcohol and drinks. Taking advantage of Gypsy's offerings is like having a personal cocktail bar that comes to You! These are no ordinary bartenders, but experienced and passionate experts who are dedicated to providing You and your guests with an unforgettable experience. Whether it's a wedding, corporate event, private party or festival, we will bring your event to life with a customised drinks menu, expertly crafted cocktails and exceptional service!";
     galleryH2.innerHTML = "Gallery";
     galleryPar.innerHTML = "Check my previous realizations";
     contactParagraphOne.innerHTML = "Check facebook for new posts";
     contactParagraphTwo.innerHTML = "Instagram for photos&relations";
-    contactH3.innerHTML = "Lorem ipsum lorem ipsum";
+    contactH3.innerHTML = "You can also write here </br> straight ahead";
     submit.innerHTML = "Send";
     namePlaceholder.placeholder = "Your name";
     emailPlaceholder.placeholder = "Your email";
